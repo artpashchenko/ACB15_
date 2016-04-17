2f18a4ba6256502c725b1ab3ca1edcc4ef66ff1c
artpashchenko <pashchenko.artem.1988@gmail.com>
2016-04-17 14:11:24 +0300

    home

---
+++ src/Week2/home/Home.java
@@ -0,0 +1,14 @@
+package Week2.home;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Артем
+ * Date: 17.04.16
+ * Time: 14:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Home {
+    public static void main(String[] args) {
+
+    }
+}
