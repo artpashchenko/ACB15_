596dee46824732e0dc5f55357ee67590b948860b
artpashchenko <pashchenko.artem.1988@gmail.com>
2016-04-22 07:44:12 +0300

    home

---
+++ src/Week2/HomeWork/_03HomeWeek2.java
@@ -0,0 +1,14 @@
+package Week2.HomeWork;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Артем
+ * Date: 22.04.16
+ * Time: 7:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class _03HomeWeek2 {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+}
